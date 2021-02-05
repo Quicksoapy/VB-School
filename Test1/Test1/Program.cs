553add7acfd0486612e8ceaa763a45efc836d680
Quicksoapy <73587977+Quicksoapy@users.noreply.github.com>
2021-02-05 08:59:49 +0100

    test

---
+++ Test1/Test1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test1
+{
+ 
+
+    public class Hello
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Hello C# World :-)");
+        }
+    }
+
+}
+}
