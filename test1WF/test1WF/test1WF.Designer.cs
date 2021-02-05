4c6eb57b566f82d8dfd265f6d42c8a0e088e226d
Quicksoapy <73587977+Quicksoapy@users.noreply.github.com>
2021-02-05 17:33:13 +0100

    aaaa

--- test1WF/test1WF/test1WF.Designer.cs
+++ test1WF/test1WF/test1WF.Designer.cs
@@ -41,9 +41,10 @@
             this.btnStart.BackColor = System.Drawing.Color.Transparent;
             this.btnStart.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F);
             this.btnStart.ForeColor = System.Drawing.Color.Black;
-            this.btnStart.Location = new System.Drawing.Point(12, 12);
+            this.btnStart.Location = new System.Drawing.Point(9, 10);
+            this.btnStart.Margin = new System.Windows.Forms.Padding(2);
             this.btnStart.Name = "btnStart";
-            this.btnStart.Size = new System.Drawing.Size(229, 74);
+            this.btnStart.Size = new System.Drawing.Size(172, 60);
             this.btnStart.TabIndex = 5;
             this.btnStart.Text = "Start";
             this.btnStart.UseVisualStyleBackColor = false;
@@ -54,9 +55,10 @@
             this.btnClose.BackColor = System.Drawing.Color.Transparent;
             this.btnClose.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F);
             this.btnClose.ForeColor = System.Drawing.Color.Black;
-            this.btnClose.Location = new System.Drawing.Point(541, 12);
+            this.btnClose.Location = new System.Drawing.Point(406, 10);
+            this.btnClose.Margin = new System.Windows.Forms.Padding(2);
             this.btnClose.Name = "btnClose";
-            this.btnClose.Size = new System.Drawing.Size(229, 74);
+            this.btnClose.Size = new System.Drawing.Size(172, 60);
             this.btnClose.TabIndex = 6;
             this.btnClose.Text = "Close";
             this.btnClose.UseVisualStyleBackColor = false;
@@ -66,9 +68,10 @@
             // 
             this.lblMadeby.AutoSize = true;
             this.lblMadeby.BackColor = System.Drawing.Color.Transparent;
-            this.lblMadeby.Location = new System.Drawing.Point(9, 327);
+            this.lblMadeby.Location = new System.Drawing.Point(7, 266);
+            this.lblMadeby.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lblMadeby.Name = "lblMadeby";
-            this.lblMadeby.Size = new System.Drawing.Size(203, 17);
+            this.lblMadeby.Size = new System.Drawing.Size(155, 13);
             this.lblMadeby.TabIndex = 7;
             this.lblMadeby.Text = "Made by Quicksoapy, using C#";
             // 
@@ -76,9 +79,10 @@
             // 
             this.lblPic.BackColor = System.Drawing.Color.Transparent;
             this.lblPic.ForeColor = System.Drawing.Color.RoyalBlue;
-            this.lblPic.Location = new System.Drawing.Point(582, 327);
+            this.lblPic.Location = new System.Drawing.Point(436, 266);
+            this.lblPic.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lblPic.Name = "lblPic";
-            this.lblPic.Size = new System.Drawing.Size(188, 28);
+            this.lblPic.Size = new System.Drawing.Size(141, 23);
             this.lblPic.TabIndex = 8;
             this.lblPic.Text = "thispersondoesnotexist.com";
             this.lblPic.Click += new System.EventHandler(this.lblPic_Click);
@@ -87,27 +91,30 @@
             // 
             this.lblPicfrom.AutoSize = true;
             this.lblPicfrom.BackColor = System.Drawing.Color.Transparent;
-            this.lblPicfrom.Location = new System.Drawing.Point(582, 310);
+            this.lblPicfrom.Location = new System.Drawing.Point(436, 252);
+            this.lblPicfrom.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lblPicfrom.Name = "lblPicfrom";
-            this.lblPicfrom.Size = new System.Drawing.Size(95, 17);
+            this.lblPicfrom.Size = new System.Drawing.Size(71, 13);
             this.lblPicfrom.TabIndex = 9;
             this.lblPicfrom.Text = "Pictures from:";
             // 
             // test1WF
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
             this.BackgroundImage = global::test1WF.Properties.Resources.autism;
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.ClientSize = new System.Drawing.Size(782, 353);
+            this.ClientSize = new System.Drawing.Size(586, 287);
             this.Controls.Add(this.lblPicfrom);
             this.Controls.Add(this.lblPic);
             this.Controls.Add(this.lblMadeby);
             this.Controls.Add(this.btnClose);
             this.Controls.Add(this.btnStart);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "test1WF";
-            this.Text = "The Autismgenerator";
+            this.Text = "The Autism Generator";
             this.TransparencyKey = System.Drawing.Color.White;
             this.ResumeLayout(false);
             this.PerformLayout();
