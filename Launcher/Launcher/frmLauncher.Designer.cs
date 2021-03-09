74c0534c79275d3153833af1142003368e4847a9
Quicksoapy <73587977+Quicksoapy@users.noreply.github.com>
2021-03-09 09:44:24 +0100

    visual stuff

--- Launcher/Launcher/frmLauncher.Designer.cs
+++ Launcher/Launcher/frmLauncher.Designer.cs
@@ -51,9 +51,10 @@
             // lblDarkMode
             // 
             this.lblDarkMode.AutoSize = true;
-            this.lblDarkMode.Location = new System.Drawing.Point(9, 268);
+            this.lblDarkMode.Location = new System.Drawing.Point(12, 330);
+            this.lblDarkMode.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblDarkMode.Name = "lblDarkMode";
-            this.lblDarkMode.Size = new System.Drawing.Size(60, 13);
+            this.lblDarkMode.Size = new System.Drawing.Size(77, 17);
             this.lblDarkMode.TabIndex = 0;
             this.lblDarkMode.Text = "Dark Mode";
             this.lblDarkMode.Click += new System.EventHandler(this.lblDarkMode_Click);
@@ -61,18 +62,20 @@
             // lblLightMode
             // 
             this.lblLightMode.AutoSize = true;
-            this.lblLightMode.Location = new System.Drawing.Point(12, 255);
+            this.lblLightMode.Location = new System.Drawing.Point(11, 313);
+            this.lblLightMode.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblLightMode.Name = "lblLightMode";
-            this.lblLightMode.Size = new System.Drawing.Size(60, 13);
+            this.lblLightMode.Size = new System.Drawing.Size(78, 17);
             this.lblLightMode.TabIndex = 1;
             this.lblLightMode.Text = "Light Mode";
             this.lblLightMode.Click += new System.EventHandler(this.lblLightMode_Click);
             // 
             // lblReset
             // 
-            this.lblReset.Location = new System.Drawing.Point(436, 12);
+            this.lblReset.Location = new System.Drawing.Point(581, 15);
+            this.lblReset.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblReset.Name = "lblReset";
-            this.lblReset.Size = new System.Drawing.Size(87, 45);
+            this.lblReset.Size = new System.Drawing.Size(116, 55);
             this.lblReset.TabIndex = 6;
             this.lblReset.Text = "To reset the 2 custom launcher icons, click here!";
             this.lblReset.Click += new System.EventHandler(this.lblReset_Click);
@@ -81,9 +84,10 @@
             // 
             this.pbCustom2.Image = global::Launcher.Properties.Resources.custom;
             this.pbCustom2.InitialImage = null;
-            this.pbCustom2.Location = new System.Drawing.Point(330, 12);
+            this.pbCustom2.Location = new System.Drawing.Point(440, 15);
+            this.pbCustom2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.pbCustom2.Name = "pbCustom2";
-            this.pbCustom2.Size = new System.Drawing.Size(100, 100);
+            this.pbCustom2.Size = new System.Drawing.Size(133, 123);
             this.pbCustom2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pbCustom2.TabIndex = 5;
             this.pbCustom2.TabStop = false;
@@ -93,9 +97,10 @@
             // 
             this.pbCustom1.Image = global::Launcher.Properties.Resources.custom;
             this.pbCustom1.InitialImage = null;
-            this.pbCustom1.Location = new System.Drawing.Point(224, 12);
+            this.pbCustom1.Location = new System.Drawing.Point(299, 15);
+            this.pbCustom1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.pbCustom1.Name = "pbCustom1";
-            this.pbCustom1.Size = new System.Drawing.Size(100, 100);
+            this.pbCustom1.Size = new System.Drawing.Size(133, 123);
             this.pbCustom1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pbCustom1.TabIndex = 4;
             this.pbCustom1.TabStop = false;
@@ -105,9 +110,10 @@
             // 
             this.pbDiscord.Image = global::Launcher.Properties.Resources.discord_logo_png_7617;
             this.pbDiscord.InitialImage = global::Launcher.Properties.Resources._1024px_Breezeicons_apps_48_steam_svg;
-            this.pbDiscord.Location = new System.Drawing.Point(118, 12);
+            this.pbDiscord.Location = new System.Drawing.Point(157, 15);
+            this.pbDiscord.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.pbDiscord.Name = "pbDiscord";
-            this.pbDiscord.Size = new System.Drawing.Size(100, 100);
+            this.pbDiscord.Size = new System.Drawing.Size(133, 123);
             this.pbDiscord.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pbDiscord.TabIndex = 3;
             this.pbDiscord.TabStop = false;
@@ -116,9 +122,10 @@
             // 
             this.pbSteam.Image = global::Launcher.Properties.Resources._1024px_Breezeicons_apps_48_steam_svg;
             this.pbSteam.InitialImage = global::Launcher.Properties.Resources._1024px_Breezeicons_apps_48_steam_svg;
-            this.pbSteam.Location = new System.Drawing.Point(12, 12);
+            this.pbSteam.Location = new System.Drawing.Point(16, 15);
+            this.pbSteam.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.pbSteam.Name = "pbSteam";
-            this.pbSteam.Size = new System.Drawing.Size(100, 100);
+            this.pbSteam.Size = new System.Drawing.Size(133, 123);
             this.pbSteam.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pbSteam.TabIndex = 2;
             this.pbSteam.TabStop = false;
@@ -126,43 +133,50 @@
             // 
             // txtCustom1
             // 
-            this.txtCustom1.Location = new System.Drawing.Point(224, 118);
+            this.txtCustom1.Location = new System.Drawing.Point(299, 145);
+            this.txtCustom1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.txtCustom1.Name = "txtCustom1";
-            this.txtCustom1.Size = new System.Drawing.Size(100, 20);
+            this.txtCustom1.Size = new System.Drawing.Size(132, 22);
             this.txtCustom1.TabIndex = 7;
+            this.txtCustom1.Text = "Adress to .exe here!";
             // 
             // txtCustom2
             // 
-            this.txtCustom2.Location = new System.Drawing.Point(330, 118);
+            this.txtCustom2.Location = new System.Drawing.Point(440, 145);
+            this.txtCustom2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.txtCustom2.Name = "txtCustom2";
-            this.txtCustom2.Size = new System.Drawing.Size(100, 20);
+            this.txtCustom2.Size = new System.Drawing.Size(132, 22);
             this.txtCustom2.TabIndex = 8;
+            this.txtCustom2.Text = "Adress to .exe here!";
             // 
             // btnCustom1
             // 
-            this.btnCustom1.Location = new System.Drawing.Point(235, 144);
+            this.btnCustom1.Location = new System.Drawing.Point(313, 177);
+            this.btnCustom1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btnCustom1.Name = "btnCustom1";
-            this.btnCustom1.Size = new System.Drawing.Size(75, 23);
+            this.btnCustom1.Size = new System.Drawing.Size(100, 28);
             this.btnCustom1.TabIndex = 9;
-            this.btnCustom1.Text = "Confirm";
+            this.btnCustom1.Text = "Add";
             this.btnCustom1.UseVisualStyleBackColor = true;
             this.btnCustom1.Click += new System.EventHandler(this.btnCustom1_Click);
             // 
             // btnCustom2
             // 
-            this.btnCustom2.Location = new System.Drawing.Point(346, 144);
+            this.btnCustom2.Location = new System.Drawing.Point(461, 177);
+            this.btnCustom2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btnCustom2.Name = "btnCustom2";
-            this.btnCustom2.Size = new System.Drawing.Size(75, 23);
+            this.btnCustom2.Size = new System.Drawing.Size(100, 28);
             this.btnCustom2.TabIndex = 10;
-            this.btnCustom2.Text = "Confirm";
+            this.btnCustom2.Text = "Add";
             this.btnCustom2.UseVisualStyleBackColor = true;
             this.btnCustom2.Click += new System.EventHandler(this.btnCustom2_Click);
             // 
             // btnClose
             // 
-            this.btnClose.Location = new System.Drawing.Point(452, 263);
+            this.btnClose.Location = new System.Drawing.Point(603, 324);
+            this.btnClose.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btnClose.Name = "btnClose";
-            this.btnClose.Size = new System.Drawing.Size(75, 23);
+            this.btnClose.Size = new System.Drawing.Size(100, 28);
             this.btnClose.TabIndex = 11;
             this.btnClose.Text = "Close";
             this.btnClose.UseVisualStyleBackColor = true;
@@ -174,9 +188,10 @@
             // 
             // btnSave
             // 
-            this.btnSave.Location = new System.Drawing.Point(371, 263);
+            this.btnSave.Location = new System.Drawing.Point(495, 324);
+            this.btnSave.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btnSave.Name = "btnSave";
-            this.btnSave.Size = new System.Drawing.Size(75, 23);
+            this.btnSave.Size = new System.Drawing.Size(100, 28);
             this.btnSave.TabIndex = 12;
             this.btnSave.Text = "Save";
             this.btnSave.UseVisualStyleBackColor = true;
@@ -184,9 +199,9 @@
             // 
             // frmMain
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(539, 291);
+            this.ClientSize = new System.Drawing.Size(719, 358);
             this.Controls.Add(this.btnSave);
             this.Controls.Add(this.btnClose);
             this.Controls.Add(this.btnCustom2);
@@ -200,6 +215,7 @@
             this.Controls.Add(this.pbSteam);
             this.Controls.Add(this.lblLightMode);
             this.Controls.Add(this.lblDarkMode);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "frmMain";
             this.Text = "Launcher";
             this.Load += new System.EventHandler(this.frmMain_Load);
