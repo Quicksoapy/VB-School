3aaa8a7a3ffaed78bd8ddc2c3de4e2267316c01a
Quicksoapy <73587977+Quicksoapy@users.noreply.github.com>
2021-03-10 18:13:16 +0100

    aaaaaaaaaaaaaaaaaaaaaaaaa

---
+++ launcherWPF/WpfApp1/WpfApp1/optionspage.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for Options.xaml
+    /// </summary>
+    public partial class Options : Window
+    {
+        public Options(MainWindow mainWindow)
+        {
+            _mainwindow = mainWindow;
+            InitializeComponent();
+        }
+        private void rbLight(object sender, RoutedEventArgs e)
+        {
+            this.Background = new SolidColorBrush(Color.FromRgb(186, 182, 247));
+            _mainwindow.Background = new SolidColorBrush(Color.FromRgb(186, 182, 247));
+        }
+
+        private void rbDark(object sender, RoutedEventArgs e)
+        {
+            this.Background = new SolidColorBrush(Color.FromRgb(0, 4, 37));
+            _mainwindow.Background = new SolidColorBrush(Color.FromRgb(0, 4, 37));
+        }
+        MainWindow _mainwindow;
+
+    }
+}
